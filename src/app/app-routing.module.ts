89fc710ff538eb391db1b1663f2e1e3e4c88ab20
oyrodriguez <oyrodriguez@worldoffice.com.co>
2024-06-04 19:20:40 -0500

    Commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TracksPageComponent } from '@modules/tracks/pages/tracks-page/tracks-page.component';
+import { TracksModule } from './modules/tracks/tracks.module';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: 'auth', //TODO (Public) Login, Register, Forgot...
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    component: HomePageComponent,
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/tracks/tracks.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TracksRoutingModule } from './tracks-routing.module';
+import { TracksPageComponent } from './pages/tracks-page/tracks-page.component';
+
+
+@NgModule({
+  declarations: [TracksPageComponent],
+  imports: [
+    CommonModule,
+    TracksRoutingModule
+  ],
+  exports: [TracksPageComponent]
+})
+export class TracksModule { }
